9953dfce8bcd1a12c51c4abc5db5867f2087ce48
Darryl Nichols <darryl.nichols@simfoods.com>
2023-08-14 13:09:27 -0600

    test

--- backend/task_manager_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ("title", "completed")
-
-
-admin.site.register(Task, TaskAdmin)
